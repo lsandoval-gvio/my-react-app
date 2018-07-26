4cdce0df87d7c1352e51130a674a0c1e3ab6b577
Luis Sandoval <lsandoval@GVio-LS.local>
2018-07-26 14:19:28 -0400

    watch video

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'GroupVisual.io',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Brilliant Design for Digital Products</h1>
+    <p>Together we solve complex design challenges and humanize smart technology.</p>
+    <Link to="/page-2/">Watch the video</Link>
+  </div>
+)
+
+export default IndexPage
